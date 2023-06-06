ba280aea8526ecc4e5abc6e2f9b8eceb0977137b
sairaj satarkar <satarkarsairaj3717@gmail.com>
2023-06-06 13:51:12 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,28 @@
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.home__data, .home__img,
+            .services__content, .menu__content,
+            .contact__data, .contact__button,
+            .footer__content`, {
+    interval: 200
+})
